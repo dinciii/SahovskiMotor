9b88050edd2fd00f2bcb5295e00f6ef75a8fda8a
dinciii <din.cufurovic@gmail.com>
2025-03-17 19:35:21 +0100

    Update AI.cs

--- AI.cs
+++ AI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,37 +8,62 @@ namespace ŠahovskiMotor
 {
     internal class AI
     {
-        //public int Minimax(Sahovnica trenutnoStanje, int depth, bool isMax)
-        //{
-        //    if (depth == 0) return 0;
+        //MINIMAX TUKAJ
+        
 
-        //    ValidatorPotez vp = new ValidatorPotez(trenutnoStanje);
-        //    if (isMax)
-        //    {
-        //        List<(int, int)> moznePoteze;
-        //        int max = int.MinValue;
-        //        foreach (TipFigure figura in Enum.GetValues(typeof(TipFigure)))
-        //        {
-        //            switch (figura)
-        //            {
-        //                case TipFigure.Kmet:
-        //                    moznePoteze = vp.PotezeKmeta(trenutnoStanje.Polja)
-        //                    break;
-        //                case TipFigure.Konj:
-        //                    break;
-        //                case TipFigure.Lovec:
-        //                    break;
-        //                case TipFigure.Top:
-        //                    break;
-        //                case TipFigure.Kraljica:
-        //                    break;
-        //                case TipFigure.Kralj:
-        //                    break;
-        //                default:
-        //                    break;
-        //            }
-        //        }
-        //    }
-        //}
+        // **
+
+
+        public List<(int, int, int, int)> VsePoteze(Sahovnica trenutnoStanje, bool whiteTurn)
+        {
+            List<(int, int, int, int)> moznePoteze = new List<(int, int, int, int)>();
+            ValidatorPotez vp = new ValidatorPotez(trenutnoStanje);
+            Barva trenutnaBarva = whiteTurn ? Barva.Bela : Barva.Črna;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Figura polje = trenutnoStanje.Polja[i, j];
+
+                    if (polje == null || polje.Barva != trenutnaBarva)
+                        continue;
+
+                    List<(int, int)> poteze = new List<(int, int)>();
+
+                    switch (polje.TipFigure)
+                    {
+                        case TipFigure.Kmet:
+                            poteze = vp.PotezeKmeta(i, j, trenutnaBarva);
+                            break;
+                        case TipFigure.Konj:
+                            poteze = vp.PotezeKonja(i, j, trenutnaBarva);
+                            break;
+                        case TipFigure.Lovec:
+                            poteze = vp.PotezeLovca(i, j, trenutnaBarva);
+                            break;
+                        case TipFigure.Top:
+                            poteze = vp.PotezeTopa(i, j, trenutnaBarva);
+                            break;
+                        case TipFigure.Kraljica:
+                            poteze = vp.PotezeKraljice(i, j, trenutnaBarva);
+                            break;
+                        case TipFigure.Kralj:
+                            poteze = vp.PotezeKralja(i, j, trenutnaBarva);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    foreach (var p in poteze)
+                    {
+                        moznePoteze.Add((i, j, p.Item1, p.Item2));
+                    }
+                }
+            }
+
+            return moznePoteze;
+        }
+
     }
 }
