0c4c722f48349a17badd91b49297e11f6a8aecfd
dinciii <din.cufurovic@gmail.com>
2025-03-17 19:36:27 +0100

    Update ValidatorPotez.cs

--- ValidatorPotez.cs
+++ ValidatorPotez.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,30 +16,58 @@ namespace ŠahovskiMotor
             Sahovnica = sahovnica;
         }
 
-        public List<(int, int)>  PotezeKmeta(int x, int y, Barva barva)
+        public List<(int, int)> PotezeKmeta(int x, int y, Barva barva)
         {
             List<(int, int)> poteze = new List<(int, int)>();
 
-            int smer = 0;
-            if (KmetNaZacMestu(x, y) && barva == Barva.Bela) { smer = 2; poteze.Add((x, y+1)); } 
-            else if (KmetNaZacMestu(x, y) && barva == Barva.Črna) { smer = -2; poteze.Add((x, y-1)); }
-            else if (!KmetNaZacMestu(x, y)) { smer = barva == Barva.Bela ? 1 : -1; }
+            int smer = (barva == Barva.Bela) ? 1 : -1; // Smer premika (beli gor, črni dol)
+            int zacetnaVrstica = (barva == Barva.Bela) ? 1 : 6; // Kje začne kmet
 
+            //Naprej za eno polje
             if (Sahovnica.ZnotrajSahovnice(x, y + smer) && Sahovnica.Polja[x, y + smer] == null)
+            {
                 poteze.Add((x, y + smer));
+
+                //Naprej za dve polji (samo če je na začetnem mestu in pred njim prazno)
+                if (y == zacetnaVrstica && Sahovnica.ZnotrajSahovnice(x, y + 2 * smer) && Sahovnica.Polja[x, y + 2 * smer] == null)
+                {
+                    poteze.Add((x, y + 2 * smer));
+                }
+            }
+
+            //Jemanje levo (preveri meje in nasprotnika)
+            if (Sahovnica.ZnotrajSahovnice(x - 1, y + smer))
+            {
+                Figura poljeLevo = Sahovnica.Polja[x - 1, y + smer];
+                if (poljeLevo != null && poljeLevo.Barva != barva) // Lahko jemlje samo nasprotnika
+                {
+                    poteze.Add((x - 1, y + smer));
+                }
+            }
+
+            //Jemanje desno (preveri meje in nasprotnika)
+            if (Sahovnica.ZnotrajSahovnice(x + 1, y + smer))
+            {
+                Figura poljeDesno = Sahovnica.Polja[x + 1, y + smer];
+                if (poljeDesno != null && poljeDesno.Barva != barva) // Lahko jemlje samo nasprotnika
+                {
+                    poteze.Add((x + 1, y + smer));
+                }
+            }
 
             return poteze;
         }
 
+
         public List<(int, int)> PotezeKonja(int x, int y, Barva barva)
         {
             List<(int, int)> legalnePoteze = new List<(int, int)>();
-            List<(int, int)> moznePoteze = new List<(int, int)> 
+            List<(int, int)> smer = new List<(int, int)> 
             { 
                 (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)
             };
 
-            foreach ((int dx, int dy) in moznePoteze)
+            foreach ((int dx, int dy) in smer)
             {
                 int novX = x + dx;
                 int novY = y + dy;
@@ -128,6 +156,41 @@ namespace ŠahovskiMotor
                     novY += dy;
                 }
             }
+            return legalnePoteze;
+        }
+
+        public List<(int, int)> PotezeKraljice(int x, int y, Barva barva)
+        {
+            List<(int, int)> legalnePoteze = new List<(int, int)>();
+            List<(int, int)> smer = new List<(int, int)>
+            {
+                //Smeri Topa
+                (1, 0), (0, 1), (-1, 0), (0, -1),
+                //In smeri Lovca
+                (1, 1), (1, -1), (-1, 1), (-1,-1)
+            };
+
+            legalnePoteze.AddRange(PotezeLovca(x, y, barva));
+            legalnePoteze.AddRange(PotezeTopa(x, y, barva));
+            return legalnePoteze;
+        }
+
+        public List<(int, int)> PotezeKralja(int x, int y, Barva barva)
+        {
+            List<(int, int)> legalnePoteze = new List<(int, int)>();
+            List<(int, int)> smer = new List<(int, int)>
+            {
+                (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)
+            };
+
+            foreach ((int dx, int dy) in smer)
+            {
+                int novX = x + dx;
+                int novY = y + dy;
+
+
+            }
+
             return legalnePoteze;
         }
 
